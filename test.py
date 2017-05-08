4c701f4b8a6218d881e7e03f893ac5978fd3dcf3
SrgSprinkles <evan.pringle1@gmail.com>
2017-05-08 19:50:34 -0400

    First program in python

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-x = 5
-
-print (x)
-
-sophia = 26
-
-print (sophia)
+#A first Python script
+import sys              #Load a library module
+print(sys.platform)
+print(2 ** 100)         #Raise 2 to a power
+x = 'Spam'
+print(x * 8)            #String repitition
+y = 2
+print(y * 3)            #An actual calculation
