4a3755685b925a4f63e60550a3bad71836e5d107
SrgSprinkles <evan.pringle1@gmail.com>
2017-05-09 20:39:44 -0400

    Added things

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 #A first Python script
-import sys              #Load a library module
+import sys              #Imports a Python module to fetch name of the platform
 print(sys.platform)
 print(2 ** 100)         #Raise 2 to a power
-x = 'Spam'
-print(x * 8)            #String repitition
+name = 'Spam'
+print(name * 8)            #String repitition
 y = 2
 print(y * 3)            #An actual calculation
+
+#You can use the command 'python test.py > test.txt' to save the output
+#of this file to a text file
+
+name = input()     #This asks for an input to be put into name
+print(name)        #Outputs name
+
+a = 'dead'
+b = 'parrot'
+c = 'sketch'
+print(a, b, c)
