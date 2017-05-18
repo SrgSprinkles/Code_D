1d606152e42d93ee9a1412e505b31b21d78b8b5c
SrgSprinkles <evan.pringle1@gmail.com>
2017-05-18 18:53:39 -0400

    For Loops Example

--- for_loops.py
+++ for_loops.py
@@ -1,5 +1,8 @@
 foods = ['bacon', 'tuna', 'ham', 'sausages', 'beef']
         #   0       1       2         3        4   #
+
 for f in foods[:2]:
     print(f)
     print(len(f))
+
+print(foods.count('ham'))
