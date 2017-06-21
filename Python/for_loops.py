e8db274695db36a40190fa3acf68da48db8aa64d
SrgSprinkles <evan.pringle1@gmail.com>
2017-06-21 19:26:39 -0400

    Test cpp

--- Python/for_loops.py
+++ Python/for_loops.py
@@ -4,5 +4,6 @@ foods = ['bacon', 'tuna', 'ham', 'sausages', 'beef']
 for f in foods[:2]:
     print(f)
     print(len(f))
+#whenever the tabbing ends, is when the for loop or if statement ends
 
 print(foods.count('ham'))
