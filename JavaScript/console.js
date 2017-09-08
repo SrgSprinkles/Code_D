de4f083acb0982a11b57290a8fbefe421bc3d3fe
SrgSprinkles <evan.pringle1@gmail.com>
2017-09-08 01:21:24 -0400

    javascript examples

---
+++ JavaScript/console.js
@@ -0,0 +1,8 @@
+//Before learning anything, you need to know how to print things to the console
+console.log("Hello!");
+
+
+/*
+This will output Hello!
+Your code will run without the semi-colon, but it is best practice to include it
+*/
---
+++ JavaScript/data_types.js
@@ -0,0 +1,16 @@
+/*
+Data types are the building blocks of all languages
+Below are examples of 4 primitive data types
+*/
+
+/* 1 */ console.log('New York City');
+/* 2 */ console.log(40.7);
+/* 3 */ console.log(true);
+/* 4 */ console.log(null);
+
+/*
+1. String
+2. Number
+3. Boolean
+4. No Value
+*/
